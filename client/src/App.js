0cf5756f88e5a86a9f5e245560863139060b19c1
Rizwan <rizwankhalid171@gmail.com>
2021-02-05 18:05:19 +0000

    Removed testing component

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,23 @@
-import React from 'react';
-import Test from './components/Test';
+import React, { Fragment } from 'react';
+
+import CssBaseline from '@material-ui/core/CssBaseline';
+
+import Header from './components/Header/Header';
+import Hero from './components/Hero/Hero';
+import RecipeContainer from './components/Recipes/RecipeContainer';
+import Footer from './components/Footer/Footer';
 
 const App = () => {
   return (
-    <div>
-      <Test />
-    </div>
+    <Fragment>
+      <CssBaseline />
+      <Header />
+      <main>
+        <Hero />
+        <RecipeContainer />
+      </main>
+      <Footer />
+    </Fragment>
   );
 };
 
